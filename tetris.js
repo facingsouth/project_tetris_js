bc7ad748c6694ff7316f96c106f0f7b7d34b057e
facingsouth <xin.d.liu@gmail.com>
2015-09-01 10:04:28 -0700

    setup canvas

---
+++ tetris.js
@@ -0,0 +1,32 @@
+var view = {
+  context: document.getElementById("board").getContext("2d"),
+  boardHeight: 500,
+  boardWidth: 250,
+  cellWidth: 25,
+
+  resetCanvas: function() {
+    this.context.clearRect(0, 0, this.boardWidth, this.boardHeight);
+    // horizontal lines
+    for (var i=0; i<this.boardHeight; i+=this.cellWidth) {
+      this.context.beginPath();
+      this.context.moveTo(0, i);
+      this.context.lineTo(this.boardWidth, i);
+      this.context.closePath();
+      this.context.stroke();
+    }
+
+    // vertical lines
+    for (var i=0; i<this.boardWidth; i+=this.cellWidth) {
+      this.context.beginPath();
+      this.context.moveTo(i, 0);
+      this.context.lineTo(i, this.boardHeight);
+      this.context.closePath();
+      this.context.stroke();
+    }
+  },
+
+  drawCell: function(x, y) {
+    this.context.fillStyle = "#ABCDEF";
+    this.context.fillRect(x*this.cellWidth, y*this.cellWidth, this.cellWidth, this.cellWidth);
+  }
+}
