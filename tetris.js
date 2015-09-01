0fad5e536a60b08073480642adce344d65fc70fc
Joseph Lozano <lozanojoseph@gmail.com>
2015-09-01 10:46:24 -0700

    cells falling by gravity and stopping at bottom

--- tetris.js
+++ tetris.js
@@ -1,3 +1,56 @@
+var model = {
+  activeCells: [],
+  placedCells: [],
+
+  Cell: function(){
+    this.x = Math.floor(Math.random() * view.boardWidth/view.cellWidth)
+    this.y = -1;
+    model.activeCells.push(this);
+  },
+
+  moveCellsDown: function(){
+    for (i = 0; i < model.activeCells.length; i++){
+      cell = model.activeCells[i]
+      cell.y++
+      view.drawCell(cell.x, cell.y)
+    }
+  },
+
+  stopPiece: function(){
+    console.log("stopping!")
+    model.placedCells = model.placedCells.concat(model.activeCells)
+    model.activeCells = []
+  },
+
+  checkCells: function(){
+    for (i = 0; i < model.activeCells.length; i++){
+      cell = model.activeCells[i]
+      if (cell.y == (view.boardHeight/view.cellWidth) - 1 /*OR cell below is occupado*/){
+       model.stopPiece();
+      }
+    }
+  },
+
+  deleteCells: function(row){
+    for (i = 0; i < model.activeCells.length; i++){
+      cell = model.placedCells[i]
+      if (cell.y == row){
+        model.activeCells.splice(i,1)
+      }
+    }
+  }
+  /*
+  if cell reaches bottom OR placed cell, then
+  stopCell -> move cell from ActiveCells to PlacedCells
+   */
+}
+
+var controller = {
+  gravity: function(){
+    model.moveCellsDown();
+  }
+}
+
 var view = {
   context: document.getElementById("board").getContext("2d"),
   boardHeight: 500,
@@ -23,10 +76,30 @@ var view = {
       this.context.closePath();
       this.context.stroke();
     }
+
+    view.drawPlacedCells();
+  },
+
+  drawPlacedCells: function(){
+    for (var i = model.placedCells.length - 1; i >= 0; i--) {
+      view.drawCell(model.placedCells[i].x,model.placedCells[i].y)
+    };
   },
 
   drawCell: function(x, y) {
     this.context.fillStyle = "#ABCDEF";
     this.context.fillRect(x*this.cellWidth, y*this.cellWidth, this.cellWidth, this.cellWidth);
   }
+
 }
+/*
+if row is full, the delete row
+model.deleteCells(row);
+ */
+
+
+setInterval(function(){
+  view.resetCanvas();
+  controller.gravity();
+  model.checkCells()
+}, 1000)
